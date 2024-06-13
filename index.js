a5a8621357fb4073f5d82fb47ffdf1fa255550b7
janmesh rajput <janmeshrajput1814@gmail.com>
2024-06-14 03:09:00 +0530

    completed

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+// 
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const secretKey = "secretKey";
 const port = 8080;
 const path = require("path");
 const methodOverride = require("method-override");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Blog } = require("./models/blog.js");
 const { Comment } = require("./models/blog.js");
+const User = require("./models/user.js");
+const bcrypt = require("bcryptjs");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 main()
     .then(() => {
@@ -15,20 +22,97 @@ main()
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/blogging', { useNewUrlParser: true, useUnifiedTopology: true });
-};
+}
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+
+// authentication middleware
+const authenticateToken = (req, res, next) => {
+    const token = req.cookies.token;
+    // console.log(token);
+
+    if (!token) {
+        return res.status(401).send("Access Denied");
+    }
+
+    try {
+        const verified = jwt.verify(token, secretKey);
+        req.user = verified.user;
+        next();
+    } catch (error) {
+        console.error("Invalid Token", error);
+        res.status(400).send("Invalid Token");
+    }
+}
+
+// 6-) token-based authentication
+app.get("/", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.post("/login", async(req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).send("User doesn't exist");
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).send("Password does not match");
+        }
+
+        const token = jwt.sign({ user }, secretKey, { expiresIn: "300s" });
+        res.cookie('token', token, { httpOnly: true }).redirect('/blogs');
+
+    } catch (error) {
+        res.status(500).send("Server error");
+    }
+});
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
+app.post("/register", async(req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        let user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).send("Username already taken");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user = new User({
+            username,
+            password: hashedPassword
+        });
+
+        await user.save();
+
+        const token = jwt.sign({ user }, secretKey, { expiresIn: "300s" });
+        res.cookie('token', token, { httpOnly: true }).redirect('/blogs');
+    } catch (error) {
+        res.status(500).send("Server Error");
+    }
+});
 
 // 1-) New route - to add new blog 
 app.get("/blogs/new", (req, res) => {
     res.render("new.ejs");
 });
 
-app.post('/blogs', async(req, res) => {
+app.post('/blogs', authenticateToken, async(req, res) => {
     try {
         const { title, author, content, tags } = req.body;
 
@@ -54,7 +138,7 @@ app.post('/blogs', async(req, res) => {
 });
 
 // 2-) Index route - to get data of all post
-app.get("/blogs", async(req, res) => {
+app.get("/blogs", authenticateToken, async(req, res) => {
     let blogs = await Blog.find();
     // console.log(blogs);
     res.render("index.ejs", { blogs });
@@ -97,7 +181,7 @@ app.get("/blogs/:id/edit", async(req, res) => {
     }
 });
 
-app.put('/blogs/:id', async(req, res) => {
+app.put('/blogs/:id', authenticateToken, async(req, res) => {
     const { id } = req.params;
     const { title, content, tags } = req.body;
 
@@ -121,7 +205,7 @@ app.put('/blogs/:id', async(req, res) => {
 });
 
 // 5-) Destroy route
-app.get("/blogs/:id/destroy", async(req, res) => {
+app.get("/blogs/:id/destroy", authenticateToken, async(req, res) => {
     const { id } = req.params;
 
     try {
@@ -139,7 +223,7 @@ app.get("/blogs/:id/destroy", async(req, res) => {
 })
 
 // Route to increment likes for a blog post
-app.post('/blogs/:id/like', async(req, res) => {
+app.post('/blogs/:id/like', authenticateToken, async(req, res) => {
     const { id } = req.params;
 
     try {
@@ -159,7 +243,7 @@ app.post('/blogs/:id/like', async(req, res) => {
     }
 });
 
-app.post("/blogs/:id/comment", async(req, res) => {
+app.post("/blogs/:id/comment", authenticateToken, async(req, res) => {
     const { id } = req.params;
     const { author, content } = req.body;
 
@@ -178,10 +262,6 @@ app.post("/blogs/:id/comment", async(req, res) => {
         res.status(500).send("Server error");
     }
 })
-
-app.get("/", (req, res) => {
-    res.send("Hii, blogging community");
-});
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
